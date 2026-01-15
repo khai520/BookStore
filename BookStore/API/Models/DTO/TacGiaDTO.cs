357c7f7e80b263113b8528bcd32e89fb154ee844
khai520 <45537943+khai520@users.noreply.github.com>
2026-01-15 21:16:07 +0700

    update

--- BookStore/API/Models/DTO/TacGiaDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Models.DTO
-{
-    public class TacGiaDTO
-    {
-        public Guid Id { get; set; } 
-        public string Ten { get; set; }
-        public string? Mota { get; set; }
-    }
-}
